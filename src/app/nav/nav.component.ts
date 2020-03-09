4d9090dc23d35c53e30756e69d659cc90caf681a
thiagoamd <thiagoamd@gmail.com>
2020-03-09 15:35:37 +0000

    Add Routers

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -13,7 +14,7 @@ erroLogin: any;
 NameUser: any;
 
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -25,7 +26,10 @@ NameUser: any;
       this.alertify.error(error);
       this.model.username = '';
       this.model.password = '';
-    });
+    }, () => {
+      this.router.navigate(['/members']);
+    }
+    );
   }
 
   loggedIn() {
@@ -36,6 +40,9 @@ NameUser: any;
   logout() {
     localStorage.removeItem('token');
     localStorage.removeItem('name');
+    this.model.username = '';
+    this.model.password = '';
     this.alertify.message('logged out');
+    this.router.navigate(['/home']);
   }
 }
---
+++ src/app/routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListComponent } from './list/list.component';
+import { AuthGuard } from './_guards/auth.guard';
+
+export const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'members', component: MemberListComponent, canActivate: [AuthGuard] },
+            { path: 'messages', component: MessagesComponent, canActivate: [AuthGuard] },
+            { path: 'list', component: ListComponent, canActivate: [AuthGuard] }
+        ]
+    },
+    { path: '**', redirectTo: '', pathMatch: 'full' }
+];
