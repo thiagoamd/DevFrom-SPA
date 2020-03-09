1b755146f2c1aa2de6f35868b7210b2cacf37e63
thiagoamd <thiagoamd@gmail.com>
2020-03-09 11:32:14 +0000

    Add Colors

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
 constructor(private http: HttpClient) { }
 
@@ -16,8 +19,10 @@ login(model: any) {
     map((response: any) => {
       const user = response;
       if (user) {
-        localStorage.removeItem('token');
         localStorage.setItem('token', user.token);
+        localStorage.setItem('name', user.name);
+        this.decodedToken = this.jwtHelper.decodeToken(user.token);
+        console.log(this.decodedToken);
       }
     })
   );
@@ -25,6 +30,11 @@ login(model: any) {
 
 register(model: any) {
   return this.http.post(this.baseUrl + 'register', model);
+}
+
+loggedIn() {
+  const token = localStorage.getItem('token');
+  return !this.jwtHelper.isTokenExpired(token);
 }
 
 }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,29 +10,32 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
 model: any = {};
 erroLogin: any;
+NameUser: any;
 
-  constructor(private authService: AuthService) { }
+
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   login() {
-    this.authService.login(this.model).subscribe(next =>{
-      console.log('Logged in successfully');
+    this.authService.login(this.model).subscribe(next => {
+      this.alertify.success('Logged in successfully');
     }, error => {
-      console.log('Failed to login');
+      this.alertify.error(error);
       this.model.username = '';
       this.model.password = '';
     });
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    this.NameUser = localStorage.getItem('name');
+    return this.authService.loggedIn();
   }
 
   logout() {
     localStorage.removeItem('token');
-    console.log('logged out');
+    localStorage.removeItem('name');
+    this.alertify.message('logged out');
   }
 }
